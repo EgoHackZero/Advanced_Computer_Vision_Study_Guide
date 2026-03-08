d962f026d679344ed92b05ce879f1df1987c9933
Yehor Nepokrytyi <109929572+EgoHackZero@users.noreply.github.com>
2026-03-08 18:45:44 +0100

    changed page name to "Advanced Computer Vision Study Guide"

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Nepokrytyi Yehor knowledge graph",
+    pageTitle: "Advanced Computer Vision Study Guide",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
